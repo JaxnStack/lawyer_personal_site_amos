3c680cfcfe7da481ca22dd0bb912aedc9a8c7e93
Jackson N <njihiajack7son@gmail.com>
2025-06-17 04:48:24 +0300

    created basic views, urls and templates

--- core/views.py
+++ core/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def about(request):
+    return render(request, 'about.html')
+def practice(request):
+    return render(request, 'practice.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
